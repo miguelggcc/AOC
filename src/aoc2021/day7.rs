0df4b0b56e1124fcecf935c2764928193f8d1e5d
miguelggcc <miguelt197@gmail.com>
2023-05-22 20:34:15 +0200

    day 7

--- src/aoc2021/day7.rs
+++ src/aoc2021/day7.rs
@@ -1,9 +1,35 @@
-pub fn part1(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part1(input: &str) -> i32 {
+    let mut crabs: Vec<_> = input
+        .split(',')
+        .map(|c| c.parse::<i32>().unwrap())
+        .collect();
+    crabs.sort();
+
+    let median = crabs[crabs.len() / 2];
+    crabs.iter().map(|c| (c - median).abs()).sum()
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> i32 {
+    let crabs: Vec<_> = input
+        .split(',')
+        .map(|c| c.parse::<i32>().unwrap())
+        .collect();
+    let (min, max) = crabs.iter().fold((i32::MAX, i32::MIN), |(min, max), &c| {
+        (min.min(c), max.max(c))
+    });
+
+    (min..max)
+        .map(|align| {
+            crabs
+                .iter()
+                .map(|c| {
+                    let d = (c - align).abs();
+                    d * (d + 1) / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -11,16 +37,14 @@ mod day7 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "16,1,2,0,4,2,7,1,2,14";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT), "");
+        assert_eq!(part1(INPUT), 37);
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT), "");
+        assert_eq!(part2(INPUT), 168);
     }
 }
