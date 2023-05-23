dfc9d85145cfafd4f1e65e9c91e63f07916d47f2
miguelggcc <miguelt197@gmail.com>
2023-05-24 00:26:26 +0200

    day 9

--- src/aoc2021/day9.rs
+++ src/aoc2021/day9.rs
@@ -1,9 +1,86 @@
-pub fn part1(_input: &str) -> String {
-    String::from("Not implemented")
+use std::{
+    collections::{HashSet, VecDeque},
+    iter::once,
+};
+
+pub fn part1(input: &str) -> u32 {
+    let nx = input.lines().next().unwrap().len() as i32;
+    let map: Vec<_> = input
+        .lines()
+        .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap()))
+        .collect();
+    let ny = map.len() as i32 / nx;
+
+    map.iter()
+        .enumerate()
+        .filter(|(index, h)| {
+            let x = *index as i32 % nx;
+            let y = *index as i32 / nx;
+            deltas(x,y,nx,ny)
+                .all(|(dx, dy)| map[(x + dx + (y + dy) * nx) as usize] > **h)
+        })
+        .map(|(_, h)| h + 1)
+        .sum()
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> u32 {
+    let nx = input.lines().next().unwrap().len() as i32;
+    let map: Vec<_> = input
+        .lines()
+        .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap()))
+        .collect();
+    let ny = map.len() as i32 / nx;
+
+    map.iter()
+        .enumerate()
+        .map(|(index0, h)| {
+            let x = index0 as i32 % nx;
+            let y = index0 as i32 / nx;
+            if !deltas(x, y, nx, ny).all(|(dx, dy)| map[(x + dx + (y + dy) * nx) as usize] > *h)
+            {
+                return 0;
+            }
+            let mut visited = HashSet::new();
+            visited.insert(index0);
+            let mut q = VecDeque::from_iter(once(index0));
+            while let Some(index) = q.pop_front() {
+                let x = index as i32 % nx;
+                let y = index as i32 / nx;
+                for nindex in deltas(x, y, nx, ny)
+                    .map(|(dx, dy)| (x + dx + (y + dy) * nx) as usize)
+                {
+                    if !visited.contains(&nindex)
+                        && map[nindex].saturating_sub(map[index]) >= 1
+                        && map[nindex] != 9
+                    {
+                        visited.insert(nindex);
+                        q.push_back(nindex)
+                    }
+                }
+            }
+            visited.len() as u32
+        })
+        .fold([0; 3], |mut max, basin| {
+            if basin > max[0] {
+                max.rotate_right(1);
+                max[0] = basin;
+            } else if basin > max[1] {
+                max[2] = max[1];
+                max[1] = basin;
+            } else {
+                max[2] = max[2].max(basin);
+            }
+            max
+        })
+        .iter()
+        .product()
+}
+const DELTAS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+fn deltas(x:i32,y:i32,nx:i32,ny:i32)->impl Iterator<Item = (i32,i32)>{
+    DELTAS
+                .into_iter()
+                .filter(move |(dx, dy)| x + dx >= 0 && y + dy >= 0 && x + dx < nx && y + dy < ny)
 }
 
 #[cfg(test)]
@@ -11,16 +88,18 @@ mod day9 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT), "");
+        assert_eq!(part1(INPUT), 15);
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT), "");
+        assert_eq!(part2(INPUT), 1134);
     }
 }
