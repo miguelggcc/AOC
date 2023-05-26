95ddb6f41c2cf165fdddeefb0b54730a3ada463a
miguelggcc <miguelt197@gmail.com>
2023-05-26 23:21:02 +0200

    day 13

--- src/aoc2021/day13.rs
+++ src/aoc2021/day13.rs
@@ -1,18 +1,11 @@
-use std::collections::HashSet;
-
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete,
-    combinator::{all_consuming, map},
-    sequence::{preceded, separated_pair},
-    Finish, IResult,
+    character::complete, combinator::all_consuming, sequence::separated_pair, Finish, IResult,
 };
+use std::{collections::HashSet, str::Lines};
 
 pub fn part1(input: &str) -> usize {
-    let (mut points, folds) = parse(input);
-    fold(&mut points, folds[0].clone())
-    .len()
+    let (mut points, mut folds) = parse(input);
+    fold(&mut points, folds.next().unwrap()).len()
 }
 
 pub fn part2(input: &str) -> String {
@@ -24,7 +17,7 @@ pub fn part2(input: &str) -> String {
     let (xmax, ymax) = points
         .iter()
         .fold((0, 0), |(xmax, ymax), &(x, y)| (x.max(xmax), y.max(ymax)));
-    let mut out = String::with_capacity(((1 + xmax) * ymax) as usize);
+    let mut out = String::with_capacity(((xmax + 1) * ymax) as usize);
     for y in 0..=ymax {
         for x in 0..=xmax {
             out.push(if points.contains(&(x, y)) { '#' } else { '.' });
@@ -34,55 +27,40 @@ pub fn part2(input: &str) -> String {
     out
 }
 
-fn fold(points: &mut HashSet<(u16, u16)>, f: Fold) -> HashSet<(u16, u16)> {
-    match f {
-        Fold::X(x) => {
+fn fold(points: &mut HashSet<(u16, u16)>, f: &str) -> HashSet<(u16, u16)> {
+    let mut split = f.split('=');
+    match (
+        split.next().unwrap().chars().last().unwrap(),
+        split.next().unwrap().parse::<u16>().unwrap(),
+    ) {
+        ('x', x) => {
             let (mut left, mut right): (HashSet<_>, _) = points.drain().partition(|p| p.0 <= x);
             right.drain().for_each(|p| {
                 left.insert((2 * x - p.0, p.1));
             });
             left
         }
-        Fold::Y(y) => {
+        ('y', y) => {
             let (mut up, mut down): (HashSet<_>, _) = points.drain().partition(|p| p.1 <= y);
             down.drain().for_each(|p| {
                 up.insert((p.0, 2 * y - p.1));
             });
             up
         }
+        (e, _) => panic!("Unknown character {e}"),
     }
 }
 
-#[derive(Clone)]
-enum Fold {
-    X(u16),
-    Y(u16),
-}
-
-fn parse(input: &str) -> (HashSet<(u16, u16)>, Vec<Fold>) {
+fn parse(input: &str) -> (HashSet<(u16, u16)>, Lines) {
     let mut lines = input.lines();
     let points: HashSet<_> = (&mut lines)
         .map_while(|l| all_consuming(parse_point)(l).finish().ok().map(|(_, l)| l))
         .collect();
-
-    let folds: Vec<_> = lines
-        .map(|l| all_consuming(parse_fold)(l).finish().unwrap().1)
-        .collect();
-    (points, folds)
+    (points, lines)
 }
 
 fn parse_point(input: &str) -> IResult<&str, (u16, u16)> {
     separated_pair(complete::u16, complete::char(','), complete::u16)(input)
-}
-
-fn parse_fold(input: &str) -> IResult<&str, Fold> {
-    preceded(
-        tag("fold along "),
-        alt((
-            map(preceded(tag("x="), complete::u16), Fold::X),
-            map(preceded(tag("y="), complete::u16), Fold::Y),
-        )),
-    )(input)
 }
 
 #[cfg(test)]
