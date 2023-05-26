230143210319e78dd762819bf154e9ec0bf9b77f
miguelggcc <miguelt197@gmail.com>
2023-05-26 23:52:46 +0200

    day 13 improved

--- src/aoc2021/day13.rs
+++ src/aoc2021/day13.rs
@@ -1,22 +1,23 @@
 use nom::{
     character::complete, combinator::all_consuming, sequence::separated_pair, Finish, IResult,
 };
-use std::{collections::HashSet, str::Lines};
+use std::str::Lines;
 
 pub fn part1(input: &str) -> usize {
-    let (mut points, mut folds) = parse(input);
-    fold(&mut points, folds.next().unwrap()).len()
+    let (points, mut folds) = parse(input);
+    fold(points, folds.next().unwrap()).len()
 }
 
 pub fn part2(input: &str) -> String {
     let (mut points, folds) = parse(input);
 
     for f in folds {
-        points = fold(&mut points, f);
+        points = fold(points, f);
     }
     let (xmax, ymax) = points
         .iter()
         .fold((0, 0), |(xmax, ymax), &(x, y)| (x.max(xmax), y.max(ymax)));
+
     let mut out = String::with_capacity(((xmax + 1) * ymax) as usize);
     for y in 0..=ymax {
         for x in 0..=xmax {
@@ -27,33 +28,32 @@ pub fn part2(input: &str) -> String {
     out
 }
 
-fn fold(points: &mut HashSet<(u16, u16)>, f: &str) -> HashSet<(u16, u16)> {
+fn fold(points: Vec<(u16, u16)>, f: &str) -> Vec<(u16, u16)> {
     let mut split = f.split('=');
-    match (
+    let mut v = match (
         split.next().unwrap().chars().last().unwrap(),
         split.next().unwrap().parse::<u16>().unwrap(),
     ) {
         ('x', x) => {
-            let (mut left, mut right): (HashSet<_>, _) = points.drain().partition(|p| p.0 <= x);
-            right.drain().for_each(|p| {
-                left.insert((2 * x - p.0, p.1));
-            });
+            let (mut left, right): (Vec<_>, _) = points.into_iter().partition(|p| p.0 < x);
+            left.extend(right.into_iter().map(|p| (2 * x - p.0, p.1)));
             left
         }
         ('y', y) => {
-            let (mut up, mut down): (HashSet<_>, _) = points.drain().partition(|p| p.1 <= y);
-            down.drain().for_each(|p| {
-                up.insert((p.0, 2 * y - p.1));
-            });
+            let (mut up, down): (Vec<_>, _) = points.into_iter().partition(|p| p.1 < y);
+            up.extend(down.into_iter().map(|p| (p.0, 2 * y - p.1)));
             up
         }
         (e, _) => panic!("Unknown character {e}"),
-    }
+    };
+    v.sort_unstable();
+    v.dedup();
+    v
 }
 
-fn parse(input: &str) -> (HashSet<(u16, u16)>, Lines) {
+fn parse(input: &str) -> (Vec<(u16, u16)>, Lines) {
     let mut lines = input.lines();
-    let points: HashSet<_> = (&mut lines)
+    let points: Vec<_> = (&mut lines)
         .map_while(|l| all_consuming(parse_point)(l).finish().ok().map(|(_, l)| l))
         .collect();
     (points, lines)
