c3157e1b4ec66d18d32e16cc08a28d00663e82d6
miguelggcc <miguelt197@gmail.com>
2023-05-29 22:57:42 +0200

    day 17

--- src/aoc2021/day17.rs
+++ src/aoc2021/day17.rs
@@ -1,9 +1,59 @@
-pub fn part1(_input: &str) -> String {
-    String::from("Not implemented")
+use nom::{
+    bytes::complete::tag,
+    character::complete::i32,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
+pub fn part1(input: &str) -> i32 {
+    let ((x0, x1), (y0, y1)) = parse(input).ok().unwrap().1;
+    let mut ymax = 0;
+    for vx in ((2.0 * x0 as f32).sqrt()) as i32..=x1 {
+        for vy in 0..-y0 {
+            let mut pos = (0, 0);
+            let mut v = (vx, vy);
+            while pos.0 <= x1 && pos.1 >= y0 {
+                if pos.0 >= x0 && pos.1 <= y1 {
+                    ymax = ymax.max(vy * (vy + 1) / 2);
+                    break;
+                }
+                pos = (pos.0 + v.0, pos.1 + v.1);
+                v = (v.0 - v.0.signum(), v.1 - 1);
+            }
+        }
+    }
+    ymax
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> i32 {
+    let ((x0, x1), (y0, y1)) = parse(input).ok().unwrap().1;
+    let mut total = 0;
+    for vx in ((2.0 * x0 as f32).sqrt()) as i32..=x1 {
+        for vy in y0..-y0 {
+            let mut pos = (0, 0);
+            let mut v = (vx, vy);
+            while pos.0 <= x1 && pos.1 >= y0 {
+                if pos.0 >= x0 && pos.1 <= y1 {
+                    total += 1;
+                    break;
+                }
+                pos = (pos.0 + v.0, pos.1 + v.1);
+                v = (v.0 - v.0.signum(), v.1 - 1);
+            }
+        }
+    }
+    total
+}
+
+fn parse(input: &str) -> IResult<&str, ((i32, i32), (i32, i32))> {
+    preceded(
+        tag("target area: x="),
+        separated_pair(
+            separated_pair(i32, tag(".."), i32),
+            tag(", y="),
+            separated_pair(i32, tag(".."), i32),
+        ),
+    )(input)
 }
 
 #[cfg(test)]
@@ -11,16 +61,14 @@ mod day17 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "target area: x=20..30, y=-10..-5";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT), "");
+        assert_eq!(part1(INPUT), 45);
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT), "");
+        assert_eq!(part2(INPUT), 112);
     }
 }
