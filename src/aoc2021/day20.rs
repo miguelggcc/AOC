1b8dfdc749bf674b2a8d5607430df54831d69fce
miguelggcc <miguelt197@gmail.com>
2023-06-05 09:10:03 +0200

    day 20

--- src/aoc2021/day20.rs
+++ src/aoc2021/day20.rs
@@ -1,52 +1,51 @@
+use std::iter::once;
+
 pub fn part1(input: &str) -> usize {
-    enhance(input, 2)
+    let (iea, nx, ny, grid) = parse(input);
+    enhance(iea, grid.into_iter().flatten().collect(), nx, ny, 2)
 }
 
 pub fn part2(input: &str) -> usize {
-    enhance(input, 50)
+    let (iea, nx, ny, grid) = parse(input);
+    enhance(iea, grid.into_iter().flatten().collect(), nx, ny, 50)
 }
 
-fn enhance(input: &str, steps: usize) -> usize {
-    let mut lines = input.lines();
-    let iea: Vec<_> = lines.next().unwrap().chars().map(|c| c == '#').collect();
-    assert!(lines.next().unwrap().is_empty());
-    let inside: Vec<_> = lines
-        .map(|l| {
-            let mut row = vec![false; steps];
-            row.extend(l.chars().map(|c| c == '#'));
-            row.extend(vec![false; steps]);
-            row
-        })
-        .collect();
-    let nx = inside[0].len() as isize;
-    let mut grid = vec![vec![false; nx as usize]; steps];
-    grid.extend(inside.into_iter().chain(grid.clone()));
-    let ny = grid.len() as isize;
-    let mut grid: Vec<_> = grid.into_iter().flatten().collect();
+fn enhance(iea: Vec<bool>, mut grid: Vec<bool>, mut nx: isize, mut ny: isize, ss: usize) -> usize {
     let mut copy;
-
-    for step in 0..steps as isize {
+    for step in 0..ss {
         copy = grid.clone();
+        nx += 2;
+        ny += 2;
+        grid.extend(once(false).cycle().take((2 * (nx + 2) + 2 * ny) as usize));
         for y in 0..ny {
             for x in 0..nx {
-                let index = DELTAS
-                    .iter()
-                    .map(move |(dx, dy)| (x as isize + dx, y as isize + dy))
-                    .fold(0, |acc, (x2, y2)| {
-                        acc << 1
-                            | usize::from(
-                                if x2 < 0 || x2 >= nx || y2 < 0 || y2 >= ny {
+                let index =
+                    DELTAS
+                        .iter()
+                        .map(move |(dx, dy)| (x + dx, y + dy))
+                        .fold(0, |acc, (x2, y2)| {
+                            acc << 1
+                                | usize::from(if x2 < 1 || x2 >= nx - 1 || y2 < 1 || y2 >= ny - 1 {
                                     iea[0] && step % 2 == 1
                                 } else {
-                                    copy[(x2 + nx * y2) as usize]
-                                },
-                            )
-                    });
+                                    copy[(x2 - 1 + (nx - 2) * (y2 - 1)) as usize]
+                                })
+                        });
                 grid[(x + nx * y) as usize] = iea[index];
             }
         }
     }
     grid.into_iter().filter(|&c| c).count()
+}
+
+fn parse(input: &str) -> (Vec<bool>, isize, isize, Vec<Vec<bool>>) {
+    let mut lines = input.lines();
+    let iea: Vec<_> = lines.next().unwrap().chars().map(|c| c == '#').collect();
+    assert!(lines.next().unwrap().is_empty());
+    let grid: Vec<Vec<_>> = lines
+        .map(|l| l.chars().map(|c| c == '#').collect())
+        .collect();
+    (iea, grid[0].len() as isize, grid.len() as isize, grid)
 }
 
 const DELTAS: [(isize, isize); 9] = [
