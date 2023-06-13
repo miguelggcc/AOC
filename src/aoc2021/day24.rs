ffc35da2dedbc4f576ca72fe5beeaf069d34b875
miguelggcc <miguelt197@gmail.com>
2023-06-13 09:20:36 +0200

    day 24

--- src/aoc2021/day24.rs
+++ src/aoc2021/day24.rs
@@ -8,34 +8,31 @@ use nom::{
 
 pub fn part1(input: &str) -> u64 {
     let inst = parse(input).finish().unwrap().1;
-    let mut zstack = vec![];
-    let mut digits = [9; 14];
-    for (i, v) in inst.iter().enumerate() {
-        if v.1 > 9 && v.0 == 1 {
-            zstack.push((v.2, i))
-        } else {
-            let (offset, other_i) = zstack.pop().unwrap();
-            let delta = offset + v.1;
-            if delta > 0 {
-                digits[other_i] -= delta;
-            } else {
-                digits[i] += delta;
-            }
-        }
-    }
-    digits.into_iter().fold(0, |acc, d| acc * 10 + d as u64)
+    get_nomad(inst, Mode::Max)
 }
 
 pub fn part2(input: &str) -> u64 {
     let inst = parse(input).finish().unwrap().1;
+    get_nomad(inst, Mode::Min)
+}
+
+//This solution assumes that:
+//1. Only div z A, add x B, add y C change between inp w blocks
+//2. If A=1 then B is always >9, otherwise A=26 and B is <=9
+//3. The two cases described in 2 always happen the same number of times
+
+fn get_nomad(inst: Vec<(i8, i8, i8)>, mode: Mode) -> u64 {
     let mut zstack = vec![];
-    let mut digits = [1; 14];
+    let mut digits = [mode.clone() as i8; 14];
     for (i, v) in inst.iter().enumerate() {
         if v.1 > 9 && v.0 == 1 {
             zstack.push((v.2, i))
         } else {
             let (offset, other_i) = zstack.pop().unwrap();
-            let delta = offset + v.1;
+            let delta = match mode {
+                Mode::Max => -offset - v.1,
+                Mode::Min => offset + v.1,
+            };
             if delta > 0 {
                 digits[i] += delta;
             } else {
@@ -43,19 +40,27 @@ pub fn part2(input: &str) -> u64 {
             }
         }
     }
-    digits.into_iter().fold(0, |acc, d| acc * 10 + d as u64)
+    digits
+        .into_iter()
+        .fold(0, |n, d| n * 10 + (d as i64).unsigned_abs())
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<(i32, i32, i32)>> {
+#[derive(Clone)]
+enum Mode {
+    Max = -9,
+    Min = 1,
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<(i8, i8, i8)>> {
     separated_list1(tag("\r\nmul y x\r\nadd z y"), parse_block)(input)
 }
 
-fn parse_block(input: &str) -> IResult<&str, (i32, i32, i32)> {
+fn parse_block(input: &str) -> IResult<&str, (i8, i8, i8)> {
     let (input, _) = terminated(take_until("div z "), tag("div z "))(input)?;
-    let (input, (a, b)) = separated_pair(complete::i32, tag("\r\nadd x "), complete::i32)(input)?;
+    let (input, (a, b)) = separated_pair(complete::i8, tag("\r\nadd x "), complete::i8)(input)?;
     let (input, _) = terminated(take_until("w\r\nadd y "), tag("w\r\nadd y "))(input)?;
-    let (input, c) = complete::i32(input)?;
+    let (input, c) = complete::i8(input)?;
     Ok((input, (a, b, c)))
 }
 
-//no tests since there's no example case
+//No tests since there's no example case
