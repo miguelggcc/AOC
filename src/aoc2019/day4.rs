f79322ef2b6f278fd0696dc4e14d015db3e26c72
miguelggcc <miguelt197@gmail.com>
2023-06-18 21:23:12 +0200

    day 4

--- src/aoc2019/day4.rs
+++ src/aoc2019/day4.rs
@@ -1,26 +1,43 @@
-pub fn part1(_input: &str) -> String {
-    String::from("Not implemented")
+use itertools::*;
+
+pub fn part1(input: &str) -> usize {
+    let (start, end) = input
+        .split_once('-')
+        .map(|(s, e)| (s.parse::<u32>().unwrap(), e.parse::<u32>().unwrap()))
+        .unwrap();
+
+    (start..=end)
+        .filter(|n| {
+            let digits = DIGITS.map(|d| (n / d) % 10);
+            digits.iter().tuple_windows().all(|(d0, d1)| d1 >= d0)
+                && digits.iter().tuple_windows().any(|(d0, d1)| d1 == d0)
+        })
+        .count()
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> usize {
+    let (start, end) = input
+        .split_once('-')
+        .map(|(s, e)| (s.parse::<u32>().unwrap(), e.parse::<u32>().unwrap()))
+        .unwrap();
+
+    (start..=end)
+        .filter(|n| {
+            let digits = DIGITS.map(|d| (n / d) % 10);
+            digits.iter().tuple_windows().all(|(d0, d1)| d1 >= d0) && get_repetitions(digits)
+        })
+        .count()
 }
 
-#[cfg(test)]
-mod day4 {
+const DIGITS: [u32; 6] = [100000, 10000, 1000, 100, 10, 1];
 
-    use super::*;
-
-    const INPUT: &'static str = "";
-
-    #[test]
-    #[ignore]
-    fn part_1() {
-        assert_eq!(part1(INPUT), "");
+fn get_repetitions(digits: [u32; 6]) -> bool {
+    let mut i = digits.iter();
+    while let Some(d) = i.next() {
+        let c = i.take_while_ref(|&nd| nd == d).count() + 1;
+        if c == 2 {
+            return true;
+        }
     }
-    #[test]
-    #[ignore]
-    fn part_2() {
-        assert_eq!(part2(INPUT), "");
-    }
+    false
 }
