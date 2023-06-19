17886b338fb6ddc191a8cac279157428147d0528
miguelggcc <miguelt197@gmail.com>
2023-06-19 22:38:17 +0200

    day 5

--- src/aoc2019/day4.rs
+++ src/aoc2019/day4.rs
@@ -1,10 +1,7 @@
 use itertools::*;
 
 pub fn part1(input: &str) -> usize {
-    let (start, end) = input
-        .split_once('-')
-        .map(|(s, e)| (s.parse::<u32>().unwrap(), e.parse::<u32>().unwrap()))
-        .unwrap();
+    let (start, end) = parse(input);
 
     (start..=end)
         .filter(|n| {
@@ -16,28 +13,32 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    let (start, end) = input
-        .split_once('-')
-        .map(|(s, e)| (s.parse::<u32>().unwrap(), e.parse::<u32>().unwrap()))
-        .unwrap();
+    let (start, end) = parse(input);
 
     (start..=end)
         .filter(|n| {
             let digits = DIGITS.map(|d| (n / d) % 10);
-            digits.iter().tuple_windows().all(|(d0, d1)| d1 >= d0) && get_repetitions(digits)
+            digits.iter().tuple_windows().all(|(d0, d1)| d1 >= d0) && repetitions(digits)
         })
         .count()
 }
 
 const DIGITS: [u32; 6] = [100000, 10000, 1000, 100, 10, 1];
 
-fn get_repetitions(digits: [u32; 6]) -> bool {
+fn repetitions(digits: [u32; 6]) -> bool {
     let mut i = digits.iter();
     while let Some(d) = i.next() {
-        let c = i.take_while_ref(|&nd| nd == d).count() + 1;
-        if c == 2 {
+        if i.take_while_ref(|&nd| nd == d).count() == 1 {
             return true;
         }
     }
     false
+}
+
+fn parse(input: &str) -> (u32, u32) {
+    input
+        .split('-')
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect_tuple()
+        .unwrap()
 }
--- src/aoc2019/day5.rs
+++ src/aoc2019/day5.rs
@@ -1,26 +1,86 @@
-pub fn part1(_input: &str) -> String {
-    String::from("Not implemented")
+use itertools::*;
+
+pub fn part1(input: &str) -> String {
+    let p = parse(input);
+    execute(p, 1).into_iter().map(|n| n.to_string()).collect()
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> String {
+    let p = parse(input);
+    execute(p, 5).into_iter().map(|n| n.to_string()).collect()
 }
 
-#[cfg(test)]
-mod day5 {
+fn parse(input: &str) -> Vec<i32> {
+    input
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect()
+}
 
-    use super::*;
+const DIGITS: [i32; 2] = [100, 1000];
 
-    const INPUT: &'static str = "";
+fn execute(mut p: Vec<i32>, input: i32) -> Vec<i32> {
+    let mut output = vec![];
+    let mut i = 0;
+    loop {
+        let (par0, par1) = DIGITS
+            .iter()
+            .enumerate()
+            .map(|(index, d)| {
+                if (p[i] / d) % 10 == 1 {
+                    i + index + 1
+                } else {
+                    *p.get(i + index + 1).unwrap_or(&0) as usize
+                }
+            })
+            .collect_tuple()
+            .unwrap();
 
-    #[test]
-    #[ignore]
-    fn part_1() {
-        assert_eq!(part1(INPUT), "");
-    }
-    #[test]
-    #[ignore]
-    fn part_2() {
-        assert_eq!(part2(INPUT), "");
+        let opcode = p[i] % 100;
+        let o = *p.get(i + 3).unwrap_or(&0) as usize;
+
+        i += match opcode {
+            1 => {
+                p[o] = p[par0 as usize] + p[par1 as usize];
+                4
+            }
+            2 => {
+                p[o] = p[par0 as usize] * p[par1 as usize];
+                4
+            }
+            3 => {
+                p[par0 as usize] = input;
+                2
+            }
+            4 => {
+                output.push(p[par0 as usize]);
+                2
+            }
+            5 => {
+                if p[par0 as usize] != 0 {
+                    p[par1 as usize] as usize - i
+                } else {
+                    3
+                }
+            }
+            6 => {
+                if p[par0 as usize] == 0 {
+                    p[par1 as usize] as usize - i
+                } else {
+                    3
+                }
+            }
+            7 => {
+                p[o] = i32::from(p[par0 as usize] < p[par1 as usize]);
+                4
+            }
+            8 => {
+                p[o] = i32::from(p[par0 as usize] == p[par1 as usize]);
+                4
+            }
+            99 => return output,
+            e => panic!("uknown command {e}"),
+        };
+        assert!(i < p.len());
     }
 }
