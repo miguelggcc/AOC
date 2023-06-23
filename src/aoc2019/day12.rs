1962935836f8c73293f1c9a05157b1b9abb406da
miguelggcc <miguelt197@gmail.com>
2023-06-24 00:27:49 +0200

    day 12

--- src/aoc2019/day12.rs
+++ src/aoc2019/day12.rs
@@ -9,55 +9,79 @@ use nom::{
 
 pub fn part1(input: &str) -> i32 {
     let mut coords = parse(input).unwrap().1;
-    let mut vels = vec![[0, 0, 0]; coords.len()];
     for _ in 0..1000 {
-        coords.iter().enumerate().for_each(|(i, c1)| {
-            coords.iter().enumerate().skip(i).for_each(|(j, c2)| {
-                for vi in 0..3 {
-                    match c1[vi].cmp(&c2[vi]) {
-                        std::cmp::Ordering::Greater => {
-                            vels[i][vi] -= 1;
-                            vels[j][vi] += 1
-                        }
-                        std::cmp::Ordering::Less => {
-                            vels[i][vi] += 1;
-                            vels[j][vi] -= 1
-                        }
-                        _ => (),
-                    }
-                }
-            })
-        });
-        coords
-            .iter_mut()
-            .zip(&vels)
-            .for_each(|(c3, v3)| c3.iter_mut().zip(v3).for_each(|(c, v)| *c += v));
+        for i in 0..3{
+            calculate(&mut coords, i);
+        }
     }
     coords
         .into_iter()
-        .zip(vels.into_iter())
-        .map(|(c3, v3)| {
-            c3.iter().map(|c| c.abs()).sum::<i32>() * v3.iter().map(|v| v.abs()).sum::<i32>()
+        .map(|coord| {
+            coord.iter().map(|(c, _)| c.abs()).sum::<i32>()
+                * coord.iter().map(|(_, v)| v.abs()).sum::<i32>()
         })
         .sum::<i32>()
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("Not implemented")
+pub fn part2(input: &str) -> usize {
+    let mut coords = parse(input).unwrap().1;
+    let first = coords.clone();
+    [0, 1, 2]
+        .map(|i| {
+            (1..)
+                .find(|_| { calculate(&mut coords, i);
+                coords == first
+                })
+                .unwrap()
+        })
+        .iter()
+        .fold(1, |acc, &r| acc * r / gcd(acc, r))
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<[i32; 3]>> {
+fn calculate(coords: &mut Vec<[(i32,i32);3]>, i:usize){
+    for spl in 0..coords.len() {
+        let (left, right) = coords.split_at_mut(spl);
+        let coord2 = right.first_mut().unwrap();
+        for coord1 in left {
+            let ((c1, v1), (c2, v2)) =
+                (coord1.get_mut(i).unwrap(), coord2.get_mut(i).unwrap());
+            match c1.cmp(&c2) {
+                std::cmp::Ordering::Greater => {
+                    *v1 -= 1;
+                    *v2 += 1
+                }
+                std::cmp::Ordering::Less => {
+                    *v1 += 1;
+                    *v2 -= 1
+                }
+                _ => (),
+            }
+        }
+    }
+    coords
+        .iter_mut()
+        .for_each(|coord| coord[i].0 += coord[i].1);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<[(i32, i32); 3]>> {
     separated_list1(complete::line_ending, parse_coord)(input)
 }
 
-fn parse_coord(input: &str) -> IResult<&str, [i32; 3]> {
+fn parse_coord(input: &str) -> IResult<&str, [(i32, i32); 3]> {
     map(
         tuple((
             preceded(tag("<x="), complete::i32),
             preceded(tag(", y="), complete::i32),
             delimited(tag(", z="), complete::i32, tag(">")),
         )),
-        |(x, y, z)| [x, y, z],
+        |(x, y, z)| [(x, 0), (y, 0), (z, 0)],
     )(input)
 }
 
@@ -66,18 +90,13 @@ mod day12 {
 
     use super::*;
 
-    const INPUT: &'static str = "<x=-1, y=0, z=2>
-<x=2, y=-10, z=-7>
-<x=4, y=-8, z=8>
-<x=3, y=5, z=-1>";
+    const INPUT: &'static str = "<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>";
 
     #[test]
-    fn part_1() {
-        assert_eq!(part1(INPUT), 179);
-    }
-    #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT), "");
+        assert_eq!(part2(INPUT), 4686774924);
     }
 }
