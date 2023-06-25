018affbad696d85e518d70ad1d9bded8647de2f1
miguelggcc <miguelt197@gmail.com>
2023-06-25 22:01:45 +0200

    day 14

--- src/aoc2019/day14.rs
+++ src/aoc2019/day14.rs
@@ -1,82 +1,74 @@
-use std::cmp::Ordering;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u64 {
     let reactions = parse(input);
     let mut leftovers = HashMap::new();
     n_of_ores("FUEL", 1, &reactions, &mut leftovers)
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &str) -> u64 {
     let reactions = parse(input);
     let mut leftovers: HashMap<_, _> = HashMap::new();
 
-    binary_search(0, 100_000_000, |n| {
+    let mut fuel = 1;
+    let target = 1_000_000_000_000;
+    loop {
         leftovers.clear();
-        n_of_ores("FUEL", n, &reactions, &mut leftovers).cmp(&1_000_000_000_000)
-    })
+        let ore = n_of_ores("FUEL", fuel as u64 + 1, &reactions, &mut leftovers) as u128;
+        if ore > target {
+            return fuel as u64;
+        } else {
+            fuel = (fuel + 1).max((fuel + 1) * target / ore);
+        }
+    }
 }
 
 fn n_of_ores<'a>(
     parent: &'a str,
-    acc: usize,
+    needed: u64,
     reactions: &'a Reactions,
-    leftovers: &mut HashMap<&'a str, usize>,
-) -> usize {
+    leftovers: &mut HashMap<&'a str, u64>,
+) -> u64 {
     if let Some((np, reacts)) = reactions.get(parent) {
-        let needed = match leftovers.entry(parent) {
+        let quantity = match leftovers.entry(parent) {
             Entry::Occupied(mut o) => {
-                let diff = acc.saturating_sub(*o.get());
-                *o.get_mut() = o.get_mut().saturating_sub(acc);
+                let diff = needed.saturating_sub(*o.get());
+                *o.get_mut() = o.get_mut().saturating_sub(needed);
                 diff
             }
-            Entry::Vacant(_) => acc,
+            Entry::Vacant(_) => needed,
         };
-        if needed > 0 {
-            let n = needed / np + (needed % np).min(1);
-            let produced = n * np;
-            if produced > needed {
-                *leftovers.entry(parent).or_insert(0) += produced - needed;
-            }
-            return reacts
-                .iter()
-                .map(|(nc, r)| n_of_ores(r, *nc * n, reactions, leftovers))
-                .sum();
-        } else {
+        if quantity == 0 {
             return 0;
         }
+
+        let n = quantity / np + (quantity % np).min(1); //same as .ceil()
+        *leftovers.entry(parent).or_insert(0) += n * np - quantity;
+
+        return reacts
+            .iter()
+            .map(|(nc, r)| n_of_ores(r, *nc * n, reactions, leftovers))
+            .sum();
     }
-    acc
+    needed
 }
 
-fn binary_search(mut start: usize, mut end: usize, mut f: impl FnMut(usize) -> Ordering) -> usize {
-    while start <= end {
-        let mid = (end + start) / 2;
-        match f(mid) {
-            Ordering::Greater => end = mid - 1,
-            Ordering::Less => start = mid + 1,
-            Ordering::Equal => return mid,
-        }
-    }
-    return start - 1;
-}
-
-type Reactions<'a> = HashMap<&'a str, (usize, Vec<(usize, &'a str)>)>;
+type Reactions<'a> = HashMap<&'a str, (u64, Vec<(u64, &'a str)>)>;
 fn parse(input: &str) -> Reactions {
     let mut reactions = HashMap::new();
     for l in input.lines() {
         let (lhs, rhs) = l.split_once(" => ").unwrap();
         let (n, id) = parse_pair(rhs);
-        let reacts: Vec<_> = lhs.split(", ").map(|p| parse_pair(p)).collect();
+        let reacts: Vec<_> = lhs.split(", ").map(parse_pair).collect();
         reactions.insert(id, (n, reacts));
     }
     reactions
 }
 
-fn parse_pair(p: &str) -> (usize, &str) {
+fn parse_pair(p: &str) -> (u64, &str) {
     let (n, id) = p.split_once(' ').unwrap();
-    (n.parse::<usize>().unwrap(), id)
+    (n.parse::<u64>().unwrap(), id)
 }
 
 #[cfg(test)]
