033ba7ded9b4b05dbbca44a5bcdc202e9b8eaf19
miguelggcc <miguelt197@gmail.com>
2023-06-28 18:07:23 +0200

    clippy 15

--- src/aoc2019/day15.rs
+++ src/aoc2019/day15.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{hash_map::Entry, HashMap, HashSet, VecDeque};
 
 use super::intcode::IntCode;
 
@@ -37,15 +37,15 @@ pub fn part2(input: &str) -> u32 {
     while let Some((p, computer)) = q.pop_front() {
         for (i, m) in MOVES.iter().enumerate() {
             let new_p = (p.0 + m.0, p.1 + m.1);
-            if !map.contains_key(&new_p) {
+            if let Entry::Vacant(e) = map.entry(new_p) {
                 let mut copy = computer.clone();
                 copy.execute_input(i as isize + 1);
                 match copy.output.pop().unwrap() {
                     0 => {
-                        map.insert(new_p, 0);
+                        e.insert(0);
                     }
                     1 => {
-                        map.insert(new_p, 1);
+                        e.insert(1);
                         q.push_back((new_p, copy));
                     }
                     2 => {
