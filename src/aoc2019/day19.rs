f3d17b903e7de9a72835d7269450ab396e9b8c7a
miguelggcc <miguelt197@gmail.com>
2023-07-02 17:14:14 +0200

    day 19

--- src/aoc2019/day19.rs
+++ src/aoc2019/day19.rs
@@ -15,45 +15,29 @@ pub fn part1(input: &str) -> usize {
 
 pub fn part2(input: &str) -> u32 {
     let computer = IntCode::new(input);
-    let mut s = String::new();
-    for y in 0..50 {
-        for x in 0..50 {
-            let mut copy = computer.clone();
-            copy.execute_inputs(vec![x, y]);
-            if copy.output.pop().unwrap() == 1 {
-                s.push('#');
-            } else {
-                s.push('.');
-            }
-        }
-        s.push('\n');
-    }
 
-    println!("{s}");
-    let mut v: Vec<_> = (0..10).map(|y| get_limits(y, &computer)).collect();
+    let mut v: Vec<_> = (0..100).map(|x| get_limits(x, 0, &computer)).collect();
 
-    dbg!(&v);
-
-    for y in 10.. {
-        if v[0].1 - v[9].0 >= 10 {
-            return (v[0].0 * 10_000 + y) as u32;
+    for x in 100.. {
+        if v[0].1 - v[99].0 >= 100 {
+            return (v[99].0 + (x - 100) * 10_000) as u32;
         }
         v.rotate_left(1);
-        v[9] = get_limits(y, &computer);
+        v[99] = get_limits(x, v[98].0, &computer);
     }
     panic!("point not found");
 }
 
-fn get_limits(y: isize, computer: &IntCode) -> (isize, isize) {
-    let start = (0..)
-        .find(|&x| {
+fn get_limits(x: isize, last: isize, computer: &IntCode) -> (isize, isize) {
+    let start = (last..)
+        .find(|&y| {
             let mut copy = computer.clone();
             copy.execute_inputs(vec![x, y]);
             copy.output.pop().unwrap() == 1
         })
         .unwrap();
     let end = (start..)
-        .find(|&x| {
+        .find(|&y| {
             let mut copy = computer.clone();
             copy.execute_inputs(vec![x, y]);
             copy.output.pop().unwrap() == 0
@@ -61,22 +45,3 @@ fn get_limits(y: isize, computer: &IntCode) -> (isize, isize) {
         .unwrap();
     (start, end)
 }
-
-/*#[cfg(test)]
-mod day19 {
-
-    use super::*;
-
-    const INPUT: &'static str = "";
-
-    #[test]
-    #[ignore]
-    fn part_1() {
-        assert_eq!(part1(INPUT), "");
-    }
-    #[test]
-    #[ignore]
-    fn part_2() {
-        assert_eq!(part2(INPUT), "");
-    }
-}*/
