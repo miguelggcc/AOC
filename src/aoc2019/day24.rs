4040ada8f1c6a37b1a56445751303accf62397d6
miguelggcc <miguelt197@gmail.com>
2023-07-11 00:43:40 +0200

    day 24

--- src/aoc2019/day24.rs
+++ src/aoc2019/day24.rs
@@ -1,10 +1,7 @@
-use std::collections::{ HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 
 pub fn part1(input: &str) -> u32 {
-    let mut map: Vec<_> = input
-        .lines()
-        .flat_map(|l| l.bytes().map(|b| b == b'#'))
-        .collect();
+    let mut map = parse(input);
     let mut ratings = HashSet::new();
 
     loop {
@@ -28,109 +25,81 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> usize {
-    let map: Vec<_> = input
-        .lines()
-        .flat_map(|l| l.bytes().map(|b| b == b'#'))
-        .collect();
+    let map = parse(input);
     let mut maps = HashMap::new();
     maps.insert(0i16, map);
-    maps.insert(-1i16, vec![false; NX * NY]);
-    maps.insert(1i16, vec![false; NX * NY]);
+    let mut copy = maps.clone();
 
     for _ in 0..200 {
-        let copy = maps.clone();
-        update_bugs(0, &mut maps, &copy);
+        update_bugs(&mut maps, &mut copy);
     }
     maps.into_iter()
         .map(|(_, m)| m.iter().filter(|&&b| b).count())
         .sum()
-
-        /*let mut maps: Vec<_> = maps.into_iter().collect();
-        maps.sort_by_key(|(l,_)|*l);
-        maps.into_iter().map(|(l,m)|{ println!("\nlevel : {l}"); m.chunks(NX).for_each(|c|println!("{:?}",c.iter().map(|&b|if b{'#'}else{'.'}).collect::<String>()));
-           m.iter().filter(|&&b|b).count()}).sum()*/
 }
 
 const NX: usize = 5;
 const NY: usize = 5;
+const CENTER: usize = NX / 2 + NY / 2 * NX;
+const BORDERS: [usize; 4] = [CENTER - 1, CENTER + 1, CENTER - NX, CENTER + NX];
 
-fn update_bugs(level: i16, maps: &mut HashMap<i16, Vec<bool>>, copy: &HashMap<i16, Vec<bool>>) {
-    if let Some(map) = maps.get_mut(&level) {
-        let current_copy = map.clone();
+fn update_bugs(maps: &mut HashMap<i16, Vec<bool>>, copy: &mut HashMap<i16, Vec<bool>>) {
+    let min_level = *maps.keys().min().unwrap();
+    let max_level = *maps.keys().max().unwrap();
+    copy.insert(min_level - 1, vec![false; NX * NY]);
+    copy.insert(max_level + 1, vec![false; NX * NY]);
+    maps.insert(min_level - 1, vec![false; NX * NY]);
+    maps.insert(max_level + 1, vec![false; NX * NY]);
 
-        let (left, right, up, down) = if let Some(hole) = copy.get(&(level + 1)) {
-            (
+    std::mem::swap(copy, maps);
+    for level in (min_level - 1)..=(max_level + 1) {
+        let map = maps.get_mut(&level).unwrap();
+        let hole = if let Some(hole) = copy.get(&(level + 1)) {
+            [
                 hole.iter().step_by(NX).filter(|&&b| b).count(),
                 hole.iter().skip(NX - 1).step_by(NX).filter(|&&b| b).count(),
                 hole.iter().take(NX).filter(|&&b| b).count(),
                 hole.iter().skip(NX * (NY - 1)).filter(|&&b| b).count(),
-            )
+            ]
         } else {
-            (0, 0, 0, 0)
+            [0, 0, 0, 0]
         };
 
-        let (west, east, north, south) = if let Some(borders) = copy.get(&(level - 1)) {
-            (
-                usize::from(borders[NX / 2 - 1 + NY / 2 * NX]),
-                usize::from(borders[NX / 2 + 1 + NY / 2 * NX]),
-                usize::from(borders[NX / 2 + (NY / 2 - 1) * NX]),
-                usize::from(borders[NX / 2 + (NY / 2 + 1) * NX]),
-            )
+        let borders = if let Some(sublevel) = copy.get(&(level - 1)) {
+            BORDERS.map(|i| usize::from(sublevel[i]))
         } else {
-            (0, 0, 0, 0)
+            [0, 0, 0, 0]
         };
-
-        for (i, b) in map
-            .iter_mut()
-            .enumerate()
-            .filter(|&(i, _)| i != NX / 2 + NY / 2 * NX)
-        {
+        let current_copy = copy.get(&level).unwrap();
+        for (i, b) in map.iter_mut().enumerate().filter(|&(i, _)| i != CENTER) {
             let mut n = get_neighbours(i).filter(|&p| current_copy[p]).count();
             if i % NX == 0 {
-                n += west
+                n += borders[0]
             }
             if i % NX == NX - 1 {
-                n += east
+                n += borders[1]
             }
             if i / NX == 0 {
-                n += north
+                n += borders[2]
             }
             if i / NX == NY - 1 {
-                n += south
+                n += borders[3]
             }
 
-            if i == NX / 2 - 1 + NY / 2 * NX {
-                n += left
-            }
-            if i == NX / 2 + 1 + NY / 2 * NX {
-                n += right
-            }
-            if i == NX / 2 + (NY / 2 - 1) * NX {
-                n += up
-            }
-            if i == NX / 2 + (NY / 2 + 1) * NX {
-                n += down
+            if let Some(index) = BORDERS.iter().position(|&b| b == i) {
+                n += hole[index]
             }
 
-            if n == 1 || (!*b && n == 2) {
+            if n == 1 || (!current_copy[i] && n == 2) {
                 *b = true;
             } else {
                 *b = false;
             }
         }
-        if level == 0 {
-            update_bugs(level - 1, maps, copy);
-            update_bugs(level + 1, maps, copy);
-        } else {
-            update_bugs(level + level.signum(), maps, copy);
-        }
-    } else {
-        maps.insert(level, vec![false; NX * NY]);
     }
 }
 
-type Point = (i8, i8);
-const DELTAS: [Point; 4] = [(0, 1), (1, 0), (-1, 0), (0, -1)];
+const DELTAS: [(i8, i8); 4] = [(0, 1), (1, 0), (-1, 0), (0, -1)];
 
 fn get_neighbours(pos: usize) -> impl Iterator<Item = usize> {
     DELTAS
@@ -142,19 +111,9 @@ fn get_neighbours(pos: usize) -> impl Iterator<Item = usize> {
         .map(move |(dx, dy)| (pos as i8 + dx + dy * NX as i8) as usize)
 }
 
-#[cfg(test)]
-mod day24 {
-
-    use super::*;
-
-    const INPUT: &'static str = "....#
-#..#.
-#.?##
-..#..
-#....";
-
-    #[test]
-    fn part_1() {
-        assert_eq!(part2(INPUT), 99);
-    }
+fn parse(input: &str) -> Vec<bool> {
+    input
+        .lines()
+        .flat_map(|l| l.bytes().map(|b| b == b'#'))
+        .collect()
 }
