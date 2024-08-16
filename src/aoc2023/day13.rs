f4704dec6d92a8e12abfdfe30b64d4f425e85561
miguelggcc <miguelt197@gmail.com>
2024-08-16 18:26:01 +0200

    day14

--- src/aoc2023/day13.rs
+++ src/aoc2023/day13.rs
@@ -1,9 +1,49 @@
-pub fn part1(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    input
+        .split("\r\n\r\n")
+        .flat_map(|s| s.split("\n\n"))
+        .map(|pattern| {
+            let (xs, ys) = parse_pattern(pattern);
+            get_reflections(xs, 0) * 100 + get_reflections(ys, 0)
+        })
+        .sum::<usize>()
 }
 
-pub fn part2(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    input
+        .split("\r\n\r\n")
+        .flat_map(|s| s.split("\n\n"))
+        .map(|pattern| {
+            let (xs, ys) = parse_pattern(pattern);
+            get_reflections(xs, 1) * 100 + get_reflections(ys, 1)
+        })
+        .sum::<usize>()
+}
+
+fn parse_pattern(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let nx = input.lines().next().unwrap().chars().count();
+    let xs: Vec<_> = input
+        .lines()
+        .map(|l| l.chars().fold(0, |acc, c| (acc << 1) + (c == '#') as u32))
+        .collect();
+    let ys: Vec<_> = (0..nx)
+        .rev()
+        .map(|i| xs.iter().fold(0, |acc, x| (acc << 1) + ((x >> i) & 1)))
+        .collect();
+    (xs, ys)
+}
+
+fn get_reflections(values: Vec<u32>, n_of_smudges: u32) -> usize {
+    (1..values.len())
+        .find(|&i| {
+            values[i..]
+                .iter()
+                .zip(values[..i].iter().rev())
+                .map(|(&x0, &x1)| (x0 ^ x1).count_ones())
+                .sum::<u32>()
+                == n_of_smudges
+        })
+        .unwrap_or(0)
 }
 
 #[cfg(test)]
@@ -11,16 +51,28 @@ mod day13 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT).to_string(), "");
+        assert_eq!(part1(INPUT).to_string(), "405");
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT).to_string(), "");
+        assert_eq!(part2(INPUT).to_string(), "400");
     }
 }
--- src/aoc2023/day14.rs
+++ src/aoc2023/day14.rs
@@ -1,9 +1,83 @@
-pub fn part1(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+use std::collections::HashMap;
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    let mut grid = Grid::parse(input);
+    grid.rotate_clockwise();
+    grid.tilt();
+    grid.load()
 }
 
-pub fn part2(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let mut grid = Grid::parse(input);
+    let mut cache = HashMap::new();
+    let mut key = 0;
+    let mut loads = Vec::with_capacity(200);
+    grid.rotate_clockwise();
+
+    let (cycles, cycles0) = (0..200)
+        .find_map(|i| {
+            for _ in 0..4 {
+                grid.tilt();
+                grid.rotate_clockwise();
+            }
+            let load = grid.load() as u64;
+            key = (key << 16) + load;
+            if let Some(i0) = cache.get(&key) {
+                return Some((i, *i0));
+            }
+            cache.insert(key, i);
+            loads.push(load);
+            None
+        })
+        .unwrap();
+    loads[cycles0 + (1000000000 - cycles0) % (cycles - cycles0) - 1]
+}
+
+struct Grid {
+    grid: Vec<u8>,
+    n: usize, //always a square
+}
+
+impl Grid {
+    fn parse(input: &str) -> Self {
+        let n = input.lines().next().unwrap().len();
+        let grid: Vec<_> = input
+            .lines()
+            .flat_map(|l| {
+                l.chars().map(|c| match c {
+                    'O' => 2,
+                    '#' => 1,
+                    _ => 0,
+                })
+            })
+            .collect();
+        Self { grid, n }
+    }
+    fn load(&self) -> usize {
+        self.grid
+            .chunks(self.n)
+            .flat_map(|s| {
+                s.iter()
+                    .enumerate()
+                    .filter(|(_, &c)| c == 2)
+                    .map(|(i, _)| 1 + i)
+            })
+            .sum()
+    }
+    fn rotate_clockwise(&mut self) {
+        for i in 0..self.n - 1 {
+            for j in i + 1..self.n {
+                self.grid.swap(i + j * self.n, j + i * self.n)
+            }
+        }
+        self.grid.chunks_mut(self.n).for_each(|s| s.reverse());
+    }
+
+    fn tilt(&mut self) {
+        self.grid
+            .chunks_mut(self.n)
+            .for_each(|row| row.split_mut(|&c| c == 1).for_each(|s| s.sort()))
+    }
 }
 
 #[cfg(test)]
@@ -11,16 +85,23 @@ mod day14 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT).to_string(), "");
+        assert_eq!(part1(INPUT).to_string(), "136");
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT).to_string(), "");
+        assert_eq!(part2(INPUT).to_string(), "64");
     }
 }
