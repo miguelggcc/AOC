85723d23b3b2c66daf66cbc4d45167fef0b29189
miguelggcc <miguelt197@gmail.com>
2025-02-06 19:01:29 +0100

    day 1 rust

--- aocpython/aoc2024/day17.py
+++ aocpython/aoc2024/day17.py
@@ -1,6 +1,44 @@
 class Day17:
     def part1(input):
-        return "Not implemented"
+        r,p = input.split('\n\n')
+        reg = [int(s[12:]) for s in r.splitlines()]
+        print(reg[0])
+        program = [int(n) for n in p[9:].split(',')]
+        out = []
+        i = 0
+        
+        def combo(op):
+            if op > 3:
+                op = reg[op-4]
+            return op
+    
+        
+        while i+1 < len(program):
+            op = program[i+1]
+            
+            match program[i]:
+                case 0:
+                    reg[0] = reg[0]>>combo(op)
+                case 1:
+                    reg[1]^=op
+                case 2:
+                    reg[1] = combo(op)%8
+                case 3:
+                    if reg[0]!=0:
+                        i = op-2
+                case 4:
+                    reg[1]^=reg[2]
+                case 5:
+                    out.append(str(combo(op)%8))
+                case 6:
+                    reg[1] = reg[0]>>combo(op)
+                case 7:
+                    reg[2] = reg[0]>>combo(op)
+            i+=2
+            print(format(reg[0], 'b'))
+        print([(format(int(o), 'b')) for o in out])
+        return ",".join(out)
 
     def part2(input):
         return "Not implemented"
+ 
--- src/aoc2024/day1.rs
+++ src/aoc2024/day1.rs
@@ -1,9 +1,34 @@
-pub fn part1(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+use std::collections::HashMap;
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    let (mut left, mut right) = input
+        .lines()
+        .fold((vec![], vec![]), |(mut l, mut r), line| {
+            let mut ns = line.split("   ");
+            l.push(ns.next().unwrap().parse::<u32>().unwrap());
+            r.push(ns.next().unwrap().parse::<u32>().unwrap());
+            (l, r)
+        });
+    left.sort();
+    right.sort();
+    left.iter()
+        .zip(right)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum::<u32>()
 }
 
-pub fn part2(_input: &str) -> impl std::fmt::Display {
-    "Not implemented"
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let (mut left, mut right) = (vec![], HashMap::new());
+    for line in input.lines() {
+        let mut ns = line.split("   ");
+        left.push(ns.next().unwrap().parse::<u32>().unwrap());
+        *right
+            .entry(ns.next().unwrap().parse::<u32>().unwrap())
+            .or_insert(0) += 1;
+    }
+    left.iter()
+        .map(|l| l * right.get(l).unwrap_or(&0))
+        .sum::<u32>()
 }
 
 #[cfg(test)]
@@ -11,16 +36,19 @@ mod day1 {
 
     use super::*;
 
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
 
     #[test]
-    #[ignore]
     fn part_1() {
-        assert_eq!(part1(INPUT).to_string(), "");
+        assert_eq!(part1(INPUT).to_string(), "11");
     }
     #[test]
-    #[ignore]
     fn part_2() {
-        assert_eq!(part2(INPUT).to_string(), "");
+        assert_eq!(part2(INPUT).to_string(), "31");
     }
 }
