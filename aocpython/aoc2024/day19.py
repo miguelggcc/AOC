6b87aa18f7b1e0be2cd28c8ed3148c59ca872c1a
miguelggcc <miguelt197@gmail.com>
2025-02-13 12:23:45 +0100

    day 19

--- aocpython/aoc2024/day19.py
+++ aocpython/aoc2024/day19.py
@@ -1,6 +1,31 @@
+from functools import cache
+
+
+def parse(input):
+    p, o = input.split('\n\n')
+    return p.split(', '),  o.splitlines()
+
+
 class Day19:
+
     def part1(input):
-        return "Not implemented"
+        patterns, onsen = parse(input)
+
+        @cache
+        def match_pattern(word):
+            for p in patterns:
+                if word.startswith(p) and match_pattern(word.removeprefix(p)):
+                    return True
+            return not word
+        return sum(map(match_pattern, onsen))
 
     def part2(input):
-        return "Not implemented"
+        patterns, onsen = parse(input)
+
+        @cache
+        def match_pattern(word):
+            ans = sum(match_pattern(word.removeprefix(p))
+                      for p in patterns if word.startswith(p))
+
+            return ans + int(not word)
+        return sum(map(match_pattern, onsen))
